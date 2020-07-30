0757cdc0ff00088320108694cdcc8662ec216f5e
Diego Chapedelaine <diegochappedelaine@users.noreply.github.com>
2020-07-30 15:40:47 +0200

    fix

--- routes/user.js
+++ routes/user.js
@@ -73,7 +73,7 @@ router.post("/user/log_in", async (req, res) => {
                     },
                 });
             } else {
-                res.status(400).json("Wrong password");
+                res.status(401).json("Wrong password");
             }
         }
     } catch (error) {
